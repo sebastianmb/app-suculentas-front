fb84e3a62c6bd1db52f839df78729800135f56f1
sebastianmb <sebastianmb88@gmail.com>
2023-05-12 23:43:07 -0500

    Inicio maquetacion seleccion pot css y html

--- src/components/Pot.js
+++ src/components/Pot.js
@@ -1,15 +1,23 @@
-import React from 'react'
+import React from 'react';
+import pot from "../assets/images/pot.jpg"
 
 const Pot = () => {
   return (
     <div id='suculentas'>
       <div className='center-suculentas'>
         <div className='mostrario'>
-          Pot
+          <img src={pot} alt='matera' className=''/>
         </div>
 
         <div className='seleccion'>
-          Seleccion
+          <span><b>Seleccione el tamaño de matera</b></span>
+          <div>
+            1
+          </div>
+          <div>
+            2
+          </div>
+
         </div>
       </div>
 
