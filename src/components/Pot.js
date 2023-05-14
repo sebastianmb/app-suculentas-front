9225670544c435ed9f7dd6d52931572af6027142
sebastianmb <sebastianmb88@gmail.com>
2023-05-13 21:33:24 -0500

    Maquetacion seleccion pot html y css

--- src/components/Pot.js
+++ src/components/Pot.js
@@ -1,21 +1,29 @@
 import React from 'react';
-import pot from "../assets/images/pot.jpg"
+import pot from "../assets/images/pot.jpg";
+import potpeq from "../assets/images/potpeq.svg";
+import potmed from "../assets/images/potmed.svg";
+import potgra from "../assets/images/potgra.svg";
 
 const Pot = () => {
   return (
     <div id='suculentas'>
       <div className='center-suculentas'>
         <div className='mostrario'>
-          <img src={pot} alt='matera' className=''/>
+          <img src={pot} alt='matera' className='' />
         </div>
 
         <div className='seleccion'>
           <span><b>Seleccione el tamaño de matera</b></span>
-          <div>
-            1
-          </div>
-          <div>
-            2
+          <div id='size-pot'>
+            <button>
+              <img src={potpeq} alt='potpeq' />
+            </button>
+            <button>
+              <img src={potmed} alt='potpeq' />
+            </button>
+            <button>
+              <img src={potgra} alt='potgra' />
+            </button>
           </div>
 
         </div>
