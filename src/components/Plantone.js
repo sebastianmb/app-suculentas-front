6aa2d089e9dae0eca1d44092e2665a43f7abb36b
sebastianmb <sebastianmb88@gmail.com>
2023-05-15 18:32:55 -0500

    Ajustes css en componente Pot inicio Plantone

--- src/components/Plantone.js
+++ src/components/Plantone.js
@@ -3,7 +3,15 @@ import React from 'react'
 const Plantone = () => {
   return (
     <div id='suculentas'>
-      
+      <div className='center-suculentas'>
+        <div className='mostrario'>
+          
+        </div>
+      </div>
+      <div className='seleccion'>
+
+      </div>
+
     </div>
   )
 }
