52906cdc57f34af8b3a9d263c7f20ba6e6818f2b
sebastianmb <sebastianmb88@gmail.com>
2023-05-15 23:49:25 -0500

    Estructura css Plantone

--- src/components/Plantone.js
+++ src/components/Plantone.js
@@ -1,16 +1,31 @@
 import React from 'react'
+import potgra from "../assets/images/potgra.svg";
 
 const Plantone = () => {
   return (
     <div id='suculentas'>
       <div className='center-suculentas'>
         <div className='mostrario'>
-          
+          <div>
+            <button>
+              <img src={potgra}/>
+            </button>
+            <button>
+              2
+            </button>
+            <button>
+              3
+            </button>
+            <button>
+              4
+            </button>
+          </div>
+        </div>
+        <div className='seleccion'>
+          <span className='suculentas-base'><b>Seleccione suculentas base</b></span>
         </div>
       </div>
-      <div className='seleccion'>
 
-      </div>
 
     </div>
   )
