25d103e2360133b47e2121bcc2f01d048f17ea7c
sebastianmb <sebastianmb88@gmail.com>
2023-06-01 23:21:37 -0500

    Ajustes componente cart

--- src/App.js
+++ src/App.js
@@ -8,13 +8,12 @@ import Slider from './components/Slider';
 import Adverstiment from './components/Adverstiment';
 import Pot from './components/Pot';
 import Footer from './components/Footer'
-import CheckboxList from './components/CheckboxList';
+
 import ConfirmButton from './components/ConfirmButton';
 import Cart from './components/Cart';
 import Products from './components/Products'
 import Navbar from './components/Navbar';
-//Imagen
-import suculenta from "./assets/images/example-sucu.jpg"
+
 
 
 
@@ -32,27 +31,6 @@ function App() {
 
   
  
-
-
-  /* Ajuste Cart
-  const [cartItems, setCartItems] = useState([]);
-
-  const addToCart = (option) => {
-    setCartItems([...cartItems, option]);
-  };
-
-  const options = [
-    { id: 1, label: 'Opción 1', checked: false, quantity: 0, image: suculenta },
-    { id: 2, label: 'Opción 2', checked: false, quantity: 0, image: suculenta },
-    { id: 3, label: 'Opción 3', checked: false, quantity: 0, image: suculenta },
-    { id: 4, label: 'Opción 1', checked: false, quantity: 0, image: suculenta },
-    { id: 5, label: 'Opción 2', checked: false, quantity: 0, image: suculenta },
-    { id: 6, label: 'Opción 3', checked: false, quantity: 0, image: suculenta }
-  ];
-
-
-
-*/
   const handleConfirm = () => {
     // Lógica a ejecutar cuando se confirma
     console.log('Confirmado');
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,19 +1,28 @@
-// Cart.js
-
 import React from 'react';
 
+
 const Cart = ({ cartItems, removeFromCart }) => {
-  if (!cartItems) {
+  if (!cartItems || cartItems.length === 0) {
     return <div>No hay elementos en el carrito.</div>;
   }
+
   return (
-    <div>
-      <h2>Cart</h2>
-      <ul>
+    <div className="cart-container">
+      <h2 className="cart-title">Cart</h2>
+      <ul className="product-list">
         {cartItems.map((item) => (
-          <li key={item.id}>
-            {item.name} - ${item.price}
-            <button onClick={() => removeFromCart(item.id)}>Remove</button>
+          <li key={item.id} className="product-item">
+            <img src={item.image} alt={item.name} className="product-image" />
+            <div className="product-info">
+              <span className="product-name">{item.name}</span>
+              <div className="product-details">
+                <span className="product-quantity">Quantity: {item.quantity}</span>
+                <span className="product-price">${item.price}</span>
+              </div>
+            </div>
+            <button onClick={() => removeFromCart(item.id)} className="remove-button">
+              Remove
+            </button>
           </li>
         ))}
       </ul>
