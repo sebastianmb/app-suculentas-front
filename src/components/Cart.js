3587b338f10d3a4d356fbadfd83b68b23b89836f
sebastianmb <sebastianmb88@gmail.com>
2023-06-19 22:37:01 -0500

    Ajuste diseño carrito

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -7,20 +7,33 @@ const Cart = ({ cartItems, removeFromCart }) => {
   }
 
   return (
-    <div className="cart-container">
-      <h2 className="cart-title">Cart</h2>
+    <div className="cart-container" >
+      <h2 className="cart-title">Orden en el Carrito</h2>
+      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', paddingLeft: '40px', paddingRight: '40px', width: '71%' }}>
+        <span >
+          Producto
+        </span>
+        <span>
+          Cantidad
+        </span>
+        <span>
+          Precio
+        </span>
+
+      </div>
       <ul className="product-list">
         {cartItems.map((item) => (
-          <li key={item.id} className="product-item">
-            <img src={item.image} alt={item.name} className="product-image" />
-            <div className="product-info">
-              <span className="product-name">{item.name}</span>
-              <div className="product-details">
-                <span className="product-quantity">Quantity: {item.quantity}</span>
-                <span className="product-price">${item.price}</span>
+
+          <li key={item.id} className="product-item" >
+            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', paddingLeft: '40px', paddingRight: '40px', width: '70%' }}>
+              <div>
+                <img src={item.image} alt={item.name} className="product-image"  />
+                <span className="product-name">{item.name}</span>
               </div>
+              <span className="product-quantity">Quantity: {item.quantity}</span>
+              <span className="product-price">${item.quantity*item.price}</span>
             </div>
-            <button onClick={() => removeFromCart(item.id)} className="remove-button">
+            <button onClick={() => removeFromCart(item.id)} className="remove-button" style={{marginLeft: '150px'}}>
               Remove
             </button>
           </li>
