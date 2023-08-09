0a3e5344da61c5ed527910666a7cb6902842b49a
sebastianmb <sebastianmb88@gmail.com>
2023-08-09 10:36:50 -0500

    ajuste final footer

--- src/App.test.js
+++ src/App.test.js
@@ -1,61 +1,18 @@
-
-
-import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import { dataContext } from './components/context/DataContext';
-import Products from './Products';
+import App from './App';
 
-describe('Products Component', () => {
-  // Mocking the data context for testing
-  const mockData = {
-    data:[
-      { "id": 1, "category":"base", "name":"Product 1", "quantity": 0, "image": "base1.jpeg" },
-      { "id": 2, "category":"base","name": "Product 2",  "quantity": 0, "image": "base2.jpeg" },
-      { "id": 3, "category":"media","name": "Product 3", "quantity": 0, "image": "media1.jpeg" },
-      { "id": 4, "category":"media","name": "Product 4", "quantity": 0, "image": "media2.jpeg" },
-      { "id": 5, "category":"media","name": "Product 5", "quantity": 0, "image": "media3.jpeg" },
-      { "id": 6, "category":"media","name": "Product 6", "quantity": 0, "image": "media4.jpeg" },
-      { "id": 7, "category":"media","name": "Product 7", "quantity": 0, "image": "media5.jpeg" },
-      { "id": 8, "category":"media","name": "Product 8", "quantity": 0, "image": "media6.jpeg" },
-      { "id": 9, "category":"alta","name": "Product 9", "quantity": 0, "image": "alta1.jpeg" },
-      { "id": 10, "category":"alta","name": "Product 10", "quantity": 0, "image": "alta2.jpeg" },
-      { "id": 11, "category":"pot","name": "small", "price": 45, "quantity":1, "image": "potpeq" },
-      { "id": 12, "category":"pot","name": "medium", "price": 60, "quantity":1, "image": "potmed" },
-      { "id": 13, "category":"pot","name": "large", "price": 80, "quantity":1, "image": "potgra" }
-     
-     
-  ]
-  };
+test('clicking Confirm button opens PaymentForm', () => {
+  render(<App />);
 
-  test('renders products correctly', () => {
-    render(
-      <dataContext.Provider value={mockData}>
-        <Products addToCart={() => {}} category="base" />
-      </dataContext.Provider>
-    );
+  // Simular agregar elementos al carrito
+  const addToCartButton = screen.getByText('Agregar al carrito');
+  fireEvent.click(addToCartButton);
 
-    // Replace this with your actual rendering logic
-    const productElements = screen.getAllByTestId('product'); // Use the appropriate test id
+  // Simular hacer clic en el botón Confirmar
+  const confirmButton = screen.getByText('Confirmar');
+  fireEvent.click(confirmButton);
 
-    expect(productElements).toHaveLength(mockData.data.length);
-  });
-
-  test('adds product to cart when "Add to Cart" is clicked', () => {
-    // Initialize a mock addToCart function
-    const addToCartMock = jest.fn();
-
-    render(
-      <dataContext.Provider value={mockData}>
-        <Products addToCart={addToCartMock} category="base" />
-      </dataContext.Provider>
-    );
-
-    // Replace this with your actual rendering and interaction logic
-    const addToCartButtons = screen.getAllByText('Add to Cart'); // Use the appropriate text
-    fireEvent.click(addToCartButtons[0]); // Click the first "Add to Cart" button
-
-    expect(addToCartMock).toHaveBeenCalledWith(mockData.data[0]); // Check if addToCart function was called with the correct product
-  });
-
-  // Add more test cases as needed
+  // Verificar que PaymentForm esté visible
+  const paymentForm = screen.getByTestId('payment-form');
+  expect(paymentForm).toBeInTheDocument();
 });
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,33 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className="footer">
-      <p>Este es el contenido del footer.</p>
+      <div className="footer-section">
+        <h2>Quiénes Somos</h2>
+        <p>
+          En Pot, somos apasionados por las suculentas y la belleza natural que aportan a nuestros espacios.
+          Nos enorgullece ofrecer una experiencia en línea única, donde puedes explorar y adquirir una amplia
+          variedad de suculentas cuidadosamente seleccionadas. Nuestra misión es brindarte la oportunidad de
+          traer la naturaleza a tu hogar de una manera fácil y accesible. ¡Bienvenido a Pot, tu destino en línea
+          para suculentas exquisitas!
+        </p>
+      </div>
+      <div className="footer-section">
+        <h3>Cómo Comprar</h3>
+        <p>Comprar en Pot es muy sencillo. Solo sigue estos pasos:</p>
+        <ol>
+          <li>Ingresa a nuestra página y explora nuestras suculentas.</li>
+          <li>Escoge el tamaño de la maceta que prefieras.</li>
+          <li>Agrega las suculentas que desees a tu carrito.</li>
+          <li>Completa tu compra y espera a que lleguen tus plantas a la puerta de tu hogar.</li>
+        </ol>
+      </div>
+      <div className="footer-section">
+        <h3>DÉJANOS UN MENSAJE</h3>
+        <p>Asesoría para comprar</p>
+        <p>Teléfono: +57 3196418371</p>
+        <p>Correo: servicioalclientepot@gmail.com</p>
+      </div>
+      
     </div>
   )
 }
