116cb4789b3173c32213b1d95b256cd2b1e037e7
sebastianmb <sebastianmb88@gmail.com>
2023-08-18 23:00:37 -0500

    integrar pasarela de pago wompi

--- src/components/PaymentForm.js
+++ src/components/PaymentForm.js
@@ -2,7 +2,18 @@ import React, { useEffect } from 'react';
 
 
 
-const PaymentForm = ({price}) => {
+const PaymentForm = ({ price }) => {
+
+
+  // Definir las variables aquí
+  const reference = Date.now().toString();
+  const publicKey = 'pub_test_wHrb6SSxOnNYI7201rvbBMW6CSa3ctIP';
+  const currency = 'COP';
+  const signatureIntegrity = 'prod_integrity_Z5mMke9x0k8gpErbDqwrJXMqsI6SFli6';
+  const redirectUrl = 'http://localhost:3000/';
+
+  const concatenatedValue = reference + publicKey + currency + signatureIntegrity + redirectUrl;
+
   useEffect(() => {
 
     if (!price || price <= 0) {
@@ -17,21 +28,21 @@ const PaymentForm = ({price}) => {
     script.dataset.render = 'button';
     script.dataset.publicKey = 'pub_test_wHrb6SSxOnNYI7201rvbBMW6CSa3ctIP';
     script.dataset.currency = 'COP';
-    script.dataset.amountInCents = (price*100).toString();
+    script.dataset.amountInCents = (price * 100).toString();
     script.dataset.reference = uniqueReference;
     script.dataset.signature_integrity = 'test_integrity_H4xVIKNnEIpsfXUoy4CfTnyQPaF4655O';
-    script.dataset.redirectUrl="http://localhost:3000/";
-     
+    script.dataset.redirectUrl = "http://localhost:3000/";
+
 
     const form = document.getElementById('payment-form');
     if (form) {
       form.appendChild(script);
-    
-    
-    };
-  
 
-   
+
+    };
+
+
+
 
     return () => {
       if (form) {
@@ -39,7 +50,7 @@ const PaymentForm = ({price}) => {
       }
     };
 
-   
+
 
   }, [price]);
 
