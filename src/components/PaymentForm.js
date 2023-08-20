d4bc36a0b1dbd0db9a212d6659d852c753dc8fcc
sebastianmb <sebastianmb88@gmail.com>
2023-08-19 22:42:28 -0500

    Corregido pasarela de pago

--- src/components/PaymentForm.js
+++ src/components/PaymentForm.js
@@ -1,26 +1,25 @@
-import React, { useEffect } from 'react';
-
-
+import React, { useEffect, useState } from 'react';
+import { SHA256 } from 'crypto-js';
 
 const PaymentForm = ({ price }) => {
-
-
-  // Definir las variables aquí
-  const reference = Date.now().toString();
-  const publicKey = 'pub_test_wHrb6SSxOnNYI7201rvbBMW6CSa3ctIP';
-  const currency = 'COP';
-  const signatureIntegrity = 'prod_integrity_Z5mMke9x0k8gpErbDqwrJXMqsI6SFli6';
-  const redirectUrl = 'http://localhost:3000/';
-
-  const concatenatedValue = reference + publicKey + currency + signatureIntegrity + redirectUrl;
+  const [hash, setHash] = useState('');
 
   useEffect(() => {
-
     if (!price || price <= 0) {
       // No muestra el formulario de pago si el precio no está definido o es cero
       return;
     }
 
+    // Definir las variables aquí
+    const reference = Date.now().toString();
+    const monto = (price * 100).toString();
+    const currency = 'COP';
+    const signatureIntegrity = 'test_integrity_H4xVIKNnEIpsfXUoy4CfTnyQPaF4655O';
+
+    const concatenatedValue = reference + monto + currency + signatureIntegrity;
+    const hashHex = SHA256(concatenatedValue).toString();
+    
+    setHash(hashHex);
 
     const uniqueReference = Date.now().toString();
     const script = document.createElement('script');
@@ -28,40 +27,31 @@ const PaymentForm = ({ price }) => {
     script.dataset.render = 'button';
     script.dataset.publicKey = 'pub_test_wHrb6SSxOnNYI7201rvbBMW6CSa3ctIP';
     script.dataset.currency = 'COP';
-    script.dataset.amountInCents = (price * 100).toString();
+    script.dataset.amountInCents = monto; // Use the calculated monto
     script.dataset.reference = uniqueReference;
-    script.dataset.signature_integrity = 'test_integrity_H4xVIKNnEIpsfXUoy4CfTnyQPaF4655O';
-    script.dataset.redirectUrl = "http://localhost:3000/";
-
+    script.dataset.signature_integrity = hashHex;
+    script.dataset.redirectUrl = 'http://localhost:3000/';
 
     const form = document.getElementById('payment-form');
     if (form) {
       form.appendChild(script);
-
-
-    };
-
-
-
+    }
 
     return () => {
       if (form) {
         form.removeChild(script);
       }
     };
-
-
-
   }, [price]);
 
   return (
-    <form id="payment-form">
-      {/* Aquí puedes agregar el contenido del formulario, como campos de entrada u otros elementos */}
-    </form>
+    <div>
+      
+      <form id="payment-form">
+        {/* Aquí puedes agregar el contenido del formulario, como campos de entrada u otros elementos */}
+      </form>
+    </div>
   );
 };
 
 export default PaymentForm;
-
-
-
